0bf50f2507d8425f3776696928bfb4a67e2c76f5
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-01-12 11:12:46 +0900

    feat: implement sfc compiler

--- packages/compiler-core/index.ts
+++ packages/compiler-core/index.ts
@@ -1,3 +1,13 @@
+import { baseCompile } from "./compile";
+import { CompilerOptions } from "./options";
+
 export * from "./codegen";
 export * from "./compile";
 export * from "./parse";
+export * from "./ast";
+
+export function compile(template: string, option?: CompilerOptions) {
+  const defaultOption: Required<CompilerOptions> = { isBrowser: true };
+  if (option) Object.assign(defaultOption, option);
+  return baseCompile(template, defaultOption);
+}
