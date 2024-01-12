0a5fabffc93821fe3a3d7ba59e57e43ac5ab694a
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-01-12 11:25:57 +0900

    feat: implement sfc renderer

--- packages/@extensions/vite-plugin-chibivue/index.ts
+++ packages/@extensions/vite-plugin-chibivue/index.ts
@@ -1,6 +1,7 @@
 import { createFilter, type Plugin } from "vite";
 import { parse } from "../../compiler-sfc";
 import { compile } from "../../compiler-dom";
+import { rewriteDefault } from "../../compiler-sfc/rewriteDefault";
 
 export default function vitePluginChibivue(): Plugin {
   const filter = createFilter(/\.vue$/);
@@ -14,13 +15,21 @@ export default function vitePluginChibivue(): Plugin {
       outputs.push("import * as ChibiVue from 'chibivue'\n");
 
       const { descriptor } = parse(code, { filename: id });
+
+      const SFC_MAIN = "_sfc_main";
+      const scriptCode = rewriteDefault(
+        descriptor.script?.content ?? "",
+        SFC_MAIN
+      );
+      outputs.push(scriptCode);
+
       const templateCode = compile(descriptor.template?.content ?? "", {
         isBrowser: false,
       });
       outputs.push(templateCode);
 
       outputs.push("\n");
-      outputs.push(`export default { render }`);
+      outputs.push(`export default { ...${SFC_MAIN}, render }`);
 
       return { code: outputs.join("\n") };
     },
--- packages/runtime-core/component.ts
+++ packages/runtime-core/component.ts
@@ -82,4 +82,9 @@ export const setupComponent = (instance: ComponentInternalInstance) => {
       instance.render = compile(template);
     }
   }
+
+  const { render } = component;
+  if (render) {
+    instance.render = render as InternalRenderFunction;
+  }
 };
